59aa58f73af183cfbbb69336305a02c74493f48e
maaxlucky <kenyangn00@gmail.com>
2024-03-26 14:31:28 +0200

    odin js exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  array.forEach((num) => sum += num)
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let sum = 1;
+  array.forEach((num) => sum *= num)
+  return sum
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if( a === 0){
+    return 1
+  }
+  else {
+    return a * factorial(a-1)
+  }
 };
 
 // Do not edit below this line
